49f1a72803e093206a76ab1cb53775e2763889ae
Simon Roberts <simon@dancingcloudservices.com>
2020-11-05 08:07:54 -0700

    Day 1 Break 2

--- src/main/java/students/School.java
+++ src/main/java/students/School.java
@@ -3,18 +3,24 @@ package students;
 import java.util.ArrayList;
 import java.util.List;
 
-interface StudentCriterion {
-  boolean test(Student s);
+@FunctionalInterface
+interface Criterion<E> {
+  boolean test(E s);
+//  void doStuff();
 }
 
-class SmartStudent implements StudentCriterion {
+class SmartStudent implements Criterion<Student> {
+  private int threshold;
+  SmartStudent(int threshold) {
+    this.threshold = threshold;
+  }
   @Override
   public boolean test(Student s) {
-    return s.getGrade() > 70;
+    return s.getGrade() > threshold;
   }
 }
 
-class EnthusiasticStudent implements StudentCriterion {
+class EnthusiasticStudent implements Criterion<Student> {
   @Override
   public boolean test(Student s) {
     return s.getCourses().size() > 2;
@@ -27,9 +33,10 @@ public class School {
   // remove non-smart?? -- no BAD IDEA
   // object as function argument "primarily for it BEHAVIOR" is called "Command" pattern
   // In "FP" it's simply referred to a (one kind of) "Higher order function"
-  public static List<Student> getByCriterion(List<Student> ls, StudentCriterion crit) {
-    List<Student> res = new ArrayList<>();
-    for (Student s : ls) {
+//  public static List<Student> getByCriterion(List<Student> ls, StudentCriterion crit) {
+  public static <E> List<E> getByCriterion(Iterable<E> ls, Criterion<E> crit) {
+    List<E> res = new ArrayList<>();
+    for (E s : ls) {
       if (crit.test(s)) {
         res.add(s);
       }
@@ -54,11 +61,12 @@ public class School {
 //    }
 //    return res;
 //  }
-  public static void show(List<Student> ls) {
-    for (Student s : ls) {
+  public static <E> void show(List<E> ls) {
+    for (E s : ls) {
         System.out.println("> " + s);
     }
   }
+
   public static void main(String[] args) {
     List<Student> roster = List.of(
         Student.of("Fred", 76, "Math", "Physics"),
@@ -70,13 +78,56 @@ public class School {
 
     // Show all the "smart" students
 //    show(getSmart(roster, 75));
-    show(getByCriterion(roster, new SmartStudent()));
+    show(getByCriterion(roster, new SmartStudent(70)));
     System.out.println("----------------------");
     // smarter still???
 //    show(getSmart(roster, 80));
-//    System.out.println("----------------------");
+    show(getByCriterion(roster, new SmartStudent(80)));
+    System.out.println("----------------------");
     // enthusiastic???
 //    show(getEnthusiastic(roster, 2));
     show(getByCriterion(roster, new EnthusiasticStudent()));
+
+    System.out.println("----------------------");
+    List<String> ls = List.of("Fred", "William", "Sheila", "Jim");
+
+    class LongString implements Criterion<String> {
+      @Override
+      public boolean test(String s) {
+        return s.length() > 4;
+      }
+    }
+    show(getByCriterion(ls, new LongString()));
+    System.out.println("----------------------");
+    class ShortString implements Criterion<String> {
+      @Override
+      public boolean test(String s) {
+        return s.length() < 5;
+      }
+    }
+    // thingy to expect = a thingy
+    // thingy to expect = pieces for building thingy
+    // compiler attempts to build "thingy" from the pices
+//    show(getByCriterion(ls, (String s) -> { return s.length() < 5; } ));
+//    show(getByCriterion(ls, (s) -> { return s.length() < 5; } ));
+    // doStuff ( (String a, String b) -> ???
+    // doStuff ( (var a, var b) -> ???
+    // doStuff ( (a, b) -> ???
+    show(getByCriterion(ls, s -> s.length() < 5 ));
+
+//    Object behavior = s -> s.length() < 5s -> s.length() < 5;
+    /*
+    If I need to build a criterion of string...
+    1) need an object
+       new XXXX
+    2) need XXXX
+       class XXXX implement Criterion<String> {
+          boolean test(String s) {  // MUST BE IMPLEMENTING AN **INTERFACE**,
+           // WITH **ONE** ABSTRACT METHOD
+          ??? ooops, I need the body...
+          }
+          ??? any other methods??? NO NOT ALLOWED
+       }
+     */
   }
 }
